973f8789be959c056ad46c63ec6a5d8404f345b2
JoaoPON <jponogueira@id.uff.br>
2026-04-21 01:56:19 -0300

    Etapa 1 - Green

---
+++ snake_funcs.py
@@ -0,0 +1,6 @@
+def inicializar_jogo(largura, altura):
+    # Fazemos exatamente o que o teste pediu, do jeito mais simples
+    return {
+        "cobra": [(10, 5)],
+        "frutas": [(15, 5)]
+    }
