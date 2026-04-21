f0c5e0937431791d058fe4c81e44f80c04bd21e8
JoaoPON <jponogueira@id.uff.br>
2026-04-21 02:45:00 -0300

    Fix Etapa 1 Test

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ def test_deve_inicializar_o_jogo_com_uma_cobra_e_uma_fruta():
     estado = inicializar_jogo(largura=20, altura=10)
 
     assert "cobra" in estado
-    assert estado["cobra"] == [(10,5),(10,4)]
+    assert estado["cobra"] == [(10,5),(9,5)]
 
     assert "frutas" in estado
     assert len(estado["frutas"]) == 1
