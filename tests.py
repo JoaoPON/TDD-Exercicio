93f79170055edda7a6f5f75d2c290aec1a1d80b1
JoaoPON <jponogueira@id.uff.br>
2026-04-21 16:10:10 -0300

    Etapa 3 - Red

--- tests.py
+++ tests.py
@@ -20,3 +20,30 @@ def test_mover_cobra():
     assert estado["cobra"][0] == (11,5)
     assert estado["cobra"][1] == (10,5)
     assert len(estado["cobra"]) == 2
+
+
+#Etapa 3 - Comer Frutas e Crescer
+def test_comer_para_poder_crescer():
+    corpo = [(7,5), (6,5), (5,5)]
+    frutas = [(8,5)]
+    corpo, frutas = snake_funcs.mover_cobra(corpo, frutas, "d")
+    assert len(corpo) == 4
+    assert len(frutas) == 0
+
+def test_nao_comer_para_nao_crescer():
+    corpo = [(7,5), (6,5), (5,5)]
+    frutas = [(1,1)]
+    corpo, frutas = snake_funcs.mover_cobra(corpo, frutas, "d")
+    assert len(corpo) == 3
+    assert len(frutas) == 1
+
+#Etapa 4 - Sistema de Colisão
+
+
+#Etapa 5 - Atravessar Paredes
+
+
+#Etapa 6 - Regras de Progressão
+
+
+#Etapa 7 - Gerar Novas Frutas
