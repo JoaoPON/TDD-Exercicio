5b77aafe965c3df701eff946d21d358018b092d4
JoaoPON <jponogueira@id.uff.br>
2026-04-22 17:01:04 -0300

    Etapa 4 - Red

--- tests.py
+++ tests.py
@@ -37,13 +37,36 @@ def test_nao_comer_para_nao_crescer():
     assert len(corpo) == 3
     assert len(frutas) == 1
 
-#Etapa 4 - Sistema de Colisão
+#Etapa 4 - Gerar Novas Frutas
+def test_gerar_frutas_deve_retornar_quantidade_correta():
+    dimensoes = (20, 10)
+    
+    corpo_pequeno = [(0,0)]
+    frutas_1 = snake_funcs.gerar_frutas(dimensoes, corpo_pequeno)
+    assert len(frutas_1) == 1
 
+    corpo_medio = [(i, 0) for i in range(10)] 
+    frutas_2 = snake_funcs.gerar_frutas(dimensoes, corpo_medio)
+    assert len(frutas_2) == 2
 
-#Etapa 5 - Atravessar Paredes
+    corpo_grande = [(i, 0) for i in range(20)]
+    frutas_3 = snake_funcs.gerar_frutas(dimensoes, corpo_grande)
+    assert len(frutas_3) == 3
+
+def test_fruta_nao_pode_nascer_dentro_da_cobra():
+    dimensoes = (2, 2)
+    corpo = [(0,0), (0,1), (1,0)]
+    
+    frutas = snake_funcs.gerar_frutas(dimensoes, corpo)
+    
+    assert frutas[0] == (1, 1)
+    assert frutas[0] not in corpo
+    
+
+#Etapa 5 - Sistema de Colisão
 
 
 #Etapa 6 - Regras de Progressão
 
 
-#Etapa 7 - Gerar Novas Frutas
+#Etapa 7 - Atravessar Paredes
