ef4d9742ae7a4a99551838ace0ec89f409277343
JoaoPON <jponogueira@id.uff.br>
2026-05-02 17:10:31 -0300

    Etapa 9 - Red
    Teste para retornar o sprite atravessando parede

--- tests.py
+++ tests.py
@@ -127,3 +127,9 @@ def test_deve_retornar_head_down():
     corpo = [(10, 7), (10, 6)]
     sprite = snake_funcs.obter_sprite(corpo, (20, 10))
     assert sprite == "head_down"
+
+
+def test_cabeca_atravessando_parede_topo():
+    corpo = [(10,9), (10,0)]
+    sprite = snake_funcs.obter_sprite(corpo, (20,10))
+    assert sprite == "head_up"
